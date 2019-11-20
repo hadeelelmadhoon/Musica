8a86b43ccefc01fb4def6565984a0753a76d345e
Hadeel Elmadhoon <helmadh@uwo.ca>
2019-11-20 16:40:51 -0500

    update controllers

--- backend/routes/items.route.js
+++ backend/routes/items.route.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 // add express validator for server side sanitization
-///const { check, validationResult } = require('express-validator');
+const { check, validationResult } = require('express-validator');
 
 // Require the controllers
-//const items_controller = require('../controllers/items.controller');
+const items_controller = require('../controllers/items.controller');
 
 module.exports = router;
