853bfbe0d62de9edda025d68d3dbfb7b03f5475f
Hadeel Elmadhoon <helmadh@uwo.ca>
2019-11-20 16:26:19 -0500

    add controller

---
+++ backend/routes/items.route.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+// add express validator for server side sanitization
+///const { check, validationResult } = require('express-validator');
+
+// Require the controllers
+//const items_controller = require('../controllers/items.controller');
+
+module.exports = router;
---
+++ backend/server.js
@@ -0,0 +1,30 @@
+// server.js
+
+const express = require('express');
+const bodyParser = require('body-parser');
+var cors = require('cors');
+
+const route = require('./routes/items.route'); // Imports routes for the users
+const app = express();
+
+// Set up mongoose connection
+const mongoose = require('mongoose');
+let dev_db_url = 'mongodb+srv://someuser:abcd1234@productstutorial-wzj0e.mongodb.net/Lab5?retryWrites=true&w=majority';
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
+mongoose.connect(mongoDB, { useNewUrlParser: true });
+mongoose.Promise = global.Promise;
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+app.use(function(req, res, next) {
+    req.db = db;
+    next();
+});
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/', route);
+
+app.listen(4000, () => console.log('Express server running on ports 4000'));
